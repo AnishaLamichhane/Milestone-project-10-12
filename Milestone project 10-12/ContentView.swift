df6879d55205370a02a566f6d06110a984289cd7
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-10 19:45:44 +0545

    Initial Commit

---
+++ Milestone project 10-12/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Milestone project 10-12
+//
+//  Created by Anisha Lamichhane on 6/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
